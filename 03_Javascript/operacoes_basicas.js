665606ffca3593cbcdfab3460388ef0549cf4b11
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-06 12:47:42 -0300

    added - 03_Javascript - Operacaoes Basicas

---
+++ 03_Javascript/operacoes_basicas.js
@@ -0,0 +1,8 @@
+const v1 = 10
+const v2 = 2
+
+console.log(`soma de v1 + v2 = ${v1 + v2}`)
+console.log(`subtracao de v1 + v2 = ${v1 - v2}`)
+console.log(`multiplicacao de v1 + v2 = ${v1 * v2}`)
+console.log(`divisao de v1 + v2 = ${v1 / v2}`)
+console.log(`resto de v1 + v2 = ${v1 % v2}`)
