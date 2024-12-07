8855920ec0962b7222c27d7057350fa31dd2d3ea
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-07 10:17:32 -0300

    added - 03_Javascript - Manipulando DOM

---
+++ 03_Javascript/dom.js
@@ -0,0 +1,5 @@
+window.document.getElementById('title').remove()  // remove Flexbox
+
+window.document.getElementsByTagName('div')  // pega todas div
+
+window.document.getElementsByTagName('div')[1].remove() // remove b
