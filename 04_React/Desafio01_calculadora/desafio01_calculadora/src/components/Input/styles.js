4e792477138af90480be72e15dafeff1f05c1449
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-10 12:13:39 -0300

    added - 04_React - desafio01-calculadora - Funcao de subtracao e proximos passos

--- 04_React/Desafio01_calculadora/desafio01_calculadora/src/components/Input/styles.js
+++ 04_React/Desafio01_calculadora/desafio01_calculadora/src/components/Input/styles.js
@@ -1,29 +1,29 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const InputContainer = styled.div`
-  width: 100%;
-  height: 75px;
-  background-color: #ccc;
-
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-
-  font-size: 24px;
-  font-family: "Roboto";
-
-  input {
     width: 100%;
     height: 75px;
-    background-color: #ccc;
-    border: 0;
+    background-color: #AAAAFF;
+
     display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    padding: 0 10px;
+    align-items: center;
+    justify-content: flex-end;
 
     font-size: 24px;
-    font-family: "Roboto";
-    color: #fff;
-  }
-`;
+    font-family: 'Roboto';
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
--- 04_React/Desafio01_calculadora/desafio01_calculadora/src/index.js
+++ 04_React/Desafio01_calculadora/desafio01_calculadora/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import GlobalSyles from './global'
+import GlobalStyles from './global';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalSyles />
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
-
